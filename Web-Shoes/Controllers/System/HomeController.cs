632e9992444f03eb074e6ccb0e32ab38a58c3216
baothaiT <baothai0225@gmail.com>
2022-03-16 02:47:23 +0700

    Add project files.

---
+++ Web-Shoes/Controllers/System/HomeController.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Web_Shoes.Data;
+using Web_Shoes.Models;
+
+namespace Web_Shoes.Controllers.System
+{
+    public class HomeController : Controller
+    {
+        // Database
+        private readonly ApplicationDbContext _context;
+        public HomeController( ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult Index()
+        {
+            // Print list product in Home Page
+            var HomeProductQuery = from a in _context.Products select a;
+            return View(HomeProductQuery);
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
